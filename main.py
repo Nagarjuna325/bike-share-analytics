9df30908d24433cf22e331c5835c3875093c03f4
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2025-09-04 15:32:06 -0500

    made changes to use embeddings model for semantic matching

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default 5000 locally, Render overrides with PORT
+    app.run(host='0.0.0.0', port=port, debug=False)
